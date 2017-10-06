443f15078b3d757d61e2d67ce69eec2c3da316bf
Thomas Nicholson <nicholmen@gmail.com>
2017-10-06 21:26:06 +0000

    changed index.js to test chalk npm

---
+++ index.js
@@ -0,0 +1,8 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+console.log(message);
+
+const chalkAnimation = require('chalk-animation');
+
+chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
